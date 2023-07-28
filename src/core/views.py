4623482faa41a50a0351e4106d2b30de3063d1ff
Dieg0saave06 <diegoosaavedra2006@gmail.com>
2023-07-27 21:33:49 -0300

    publicaciones, contacto, quienes somos

--- src/core/views.py
+++ src/core/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 # view que renderiza la pagina de inicio
 def indexView(request):
     return render(request, 'index.html', {})
+
+def contactoView(request):
+    return render(request, 'contacto/contacto.html', {})
+
+def quienesSomosView(request):
+    return render(request, 'contacto/quienes-somos.html', {})
