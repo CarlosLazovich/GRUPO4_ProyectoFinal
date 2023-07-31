8020de2a17766b028cf18b1ca2bab584da60acb9
Victoria <victoriamail9ggmail.com>
2023-07-31 15:34:03 -0300

    paginador

--- src/core/views.py
+++ src/core/views.py
@@ -5,7 +5,7 @@ from publicaciones.models import Publicaciones
 # view que renderiza la pagina de inicio
 # renderiza las 4 ultimas publicaciones en el index
 def indexView(request):
-    posteos = Publicaciones.objects.order_by('fecha')[0:4]
+    posteos = Publicaciones.objects.order_by('-fecha')[0:4]
     ctx = {'posteos': posteos}
     return render(request, 'index.html', ctx)
 
