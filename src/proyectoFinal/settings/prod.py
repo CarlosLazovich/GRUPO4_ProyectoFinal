75c77eea19ab71b2a097330635f07e39b00e60aa
Carlos Lazovich <carloslazovich123@gmail.com>
2023-08-01 23:25:01 -0300

    haciendo deploy

---
+++ src/proyectoFinal/settings/prod.py
@@ -0,0 +1,18 @@
+from .base import *
+
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'proyectoFinal',
+        'USER': 'root',
+        'PASSWORD': 'josema39938597',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+"""
